1d417cffd4dfa2184c1614120959f53978f77e2c
James Ofori Ayerakwa <58654469+jamesofoaye@users.noreply.github.com>
2022-08-20 22:08:26 +0400

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js");
 
 firebase.initializeApp({
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	apiKey: "AIzaSyBNSbsWuynoRasrDxOz5A_5sUROpipO6A8",
+	authDomain: "auth.congasgh.com",
+	projectId: "congas-webapp",
+	storageBucket: "congas-webapp.appspot.com",
+	messagingSenderId: "883325992993",
+	appId: "1:883325992993:web:e1d8d5bb51862f878be360",
 });
 
 const messaging = firebase.messaging();
